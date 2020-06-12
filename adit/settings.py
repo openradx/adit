b9eb716568c9fef6c59e13f663e73939d00b1f47
Kai Schlamp <kai.schlamp@gmail.com>
2020-06-12 11:53:20 +0000

    Setup Postgresql with Django in Gitpod
    Gitpod has a default gitpod user in the Postgresql database.

--- adit/settings.py
+++ adit/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'adit.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'adit',
+        'USER': 'gitpod',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
