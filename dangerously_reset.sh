8505756c89e2885ad0ac00f4cc21d7e3f124e028
Kai Schlamp <kai.schlamp@gmail.com>
2020-06-16 13:51:45 +0000

    Automagically create admin user

--- dangerously_reset.sh
+++ dangerously_reset.sh
@@ -5,3 +5,4 @@ find . -path "*/migrations/*.pyc" -delete
 python manage.py reset_db --noinput
 python manage.py makemigrations
 python manage.py migrate
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'kai.schlamp@med.uni-heidelberg.de', 'admin')" | python manage.py shell
