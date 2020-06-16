b1936dd12d0e5eacfb90e392b5c8c1833ba45ab3
Kai Schlamp <kai.schlamp@gmail.com>
2020-06-16 13:51:15 +0000

    Added accounts app for custom user auth

--- adit/settings.py
+++ adit/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django_extensions',
     'crispy_forms',
     'main',
+    'accounts',
     'batch_transfer'
 ]
 
@@ -127,5 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'accounts.User'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
