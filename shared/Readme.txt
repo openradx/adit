f0bc42c5002fc43760da1c4299d2d6d1d5ad431a
Kai Schlamp <kai.schlamp@gmail.com>
2020-06-27 10:58:28 +0000

    Restructuring shared

--- shared/Readme.txt
+++
@@ -1,8 +0,0 @@
-transfer:
-each pat:
-    each stud:
-        download stud in temp -> upload pseudonymized
-
-download:
-each path:
-    
--- shared/activate_env.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-N:\Anwendungen\Clink\clink_x64.exe inject > nul 2>&1
-N:\Anwendungen\Miniconda\condabin\activate dicom > nul 2>&1
