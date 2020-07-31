40732c6bff76dbc0131fb8478815551e737ea4fd
Kai Schlamp <kai.schlamp@gmail.com>
2020-07-31 21:39:25 +0000

    wip

--- selective_transfer/apps.py
+++ selective_transfer/apps.py
@@ -2,42 +2,53 @@ from django.apps import AppConfig
 from django.db.models.signals import post_migrate
 from main.site import register_main_menu_item, register_dicom_job
 
+
 class SelectiveTransferConfig(AppConfig):
-    name = 'selective_transfer'
+    name = "selective_transfer"
 
     def ready(self):
         register_app()
 
+        # Put calls to db stuff in this signal handler
+        post_migrate.connect(init_db, sender=self)
+
+
 def register_app():
-    register_main_menu_item(
-        url_name='query_studies',
-        label='Selective Transfer'
-    )
-    
+    register_main_menu_item(url_name="query_studies", label="Selective Transfer")
+
+    # pylint: disable=import-outside-toplevel
     from .models import SelectiveTransferJob
     from .views import SelectiveTransferJobDetail
+
     register_dicom_job(
         type_key=SelectiveTransferJob.JOB_TYPE,
-        type_name='Selective transfer job',
-        detail_view=SelectiveTransferJobDetail
+        type_name="Selective transfer job",
+        detail_view=SelectiveTransferJobDetail,
     )
 
-def init_db(sender, **kwargs):
+
+def init_db(**kwargs):  # pylint: disable=unused-argument
     create_group()
     create_app_settings()
 
+
 def create_group():
+    # pylint: disable=import-outside-toplevel
     from accounts.utils import create_group_with_permissions
+
     create_group_with_permissions(
-        'selective_transferrers',
+        "selective_transferrers",
         (
-            'selective_transfer.add_selectivetransferjob',
-            'selective_transfer.view_selectivetransferjob'
-        )
+            "selective_transfer.add_selectivetransferjob",
+            "selective_transfer.view_selectivetransferjob",
+        ),
     )
 
+
 def create_app_settings():
+    # pylint: disable=import-outside-toplevel
     from .models import AppSettings
+
     app_settings = AppSettings.objects.first()
     if not app_settings:
         AppSettings.objects.create()
--- selective_transfer/views.py
+++ selective_transfer/views.py
@@ -6,15 +6,21 @@ from main.mixins import OwnerRequiredMixin
 from .forms import QueryStudiesForm
 from .models import SelectiveTransferJob
 
-class QueryStudiesView(LoginRequiredMixin, PermissionRequiredMixin, FormView):
-    template_name = 'selective_transfer/query_studies_form.html'
-    form_class = QueryStudiesForm
 
-class SelectiveTransferJobCreate(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
+class QueryStudiesView(LoginRequiredMixin, PermissionRequiredMixin, FormView):
+    template_name = "selective_transfer/query_studies_form.html"
+    form_class = QueryStudiesForm
+    permission_required = "selective_transfer.add_selectivetransferjob"
+
+
+class SelectiveTransferJobCreate(
+    LoginRequiredMixin, PermissionRequiredMixin, CreateView
+):
     model = SelectiveTransferJob
+
 
 class SelectiveTransferJobDetail(LoginRequiredMixin, OwnerRequiredMixin, DetailView):
     model = SelectiveTransferJob
-    context_object_name = 'job'
-    template_name = 'selective_transfer/selective_transfer_job_detail.html'
-    owner_accessor = 'created_by'
+    context_object_name = "job"
+    template_name = "selective_transfer/selective_transfer_job_detail.html"
+    owner_accessor = "created_by"
