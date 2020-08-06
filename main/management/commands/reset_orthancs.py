9a9b63165a9bfb5bdc6cb6ceff88084b9b29d08d
Kai Schlamp <kai.schlamp@gmail.com>
2020-08-06 16:45:35 +0000

    wip

--- main/management/commands/reset_orthancs.py
+++ main/management/commands/reset_orthancs.py
@@ -1,13 +1,13 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from django.conf import settings
-from pathlib import Path
 from main.tests.helpers.orthanc_rest_handler import OrthancRestHandler
 
+
 class Command(BaseCommand):
-    help = 'Clear both Orthanc instances and upload DICOM files to Orthanc 1 instance.'
+    help = "Clear both Orthanc instances and upload DICOM files to Orthanc 1 instance."
 
     def handle(self, *args, **options):
-        dicoms_folder = settings.BASE_DIR / '_debug' / 'samples' / 'dicoms'
+        dicoms_folder = settings.BASE_DIR / "samples" / "dicoms"
         handler = OrthancRestHandler(port=6501)
         handler.clear()
         handler.upload_files(dicoms_folder)
