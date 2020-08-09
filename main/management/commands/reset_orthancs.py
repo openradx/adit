d90354bc9acd72c0f1c10dd10bc07b3731bb4ef3
Kai Schlamp <kai.schlamp@gmail.com>
2020-08-09 21:12:29 +0000

    wip

--- main/management/commands/reset_orthancs.py
+++ main/management/commands/reset_orthancs.py
@@ -1,6 +1,6 @@
-import os
 from django.core.management.base import BaseCommand
 from django.conf import settings
+from adit.settings.base import env  # pylint: disable=import-error
 
 # pylint: disable=import-error,no-name-in-module
 from main.tests.helpers.orthanc_rest_handler import OrthancRestHandler
@@ -11,15 +11,13 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         dicoms_folder = settings.BASE_DIR / "samples" / "dicoms"
-        orthanc1_host = os.environ.get("ORTHANC1_HOST")
-        if not orthanc1_host:
-            orthanc1_host = "localhost"
+
+        orthanc1_host = env.str("ORTHANC1_HOST", default="localhost")
         handler = OrthancRestHandler(host=orthanc1_host, port=6501)
         handler.clear()
+
         handler.upload_files(dicoms_folder)
 
-        orthanc2_host = os.environ.get("ORTHANC2_HOST")
-        if not orthanc2_host:
-            orthanc1_host = "localhost"
+        orthanc2_host = env.str("ORTHANC2_HOST", default="localhost")
         handler = OrthancRestHandler(host=orthanc2_host, port=6502)
         handler.clear()
