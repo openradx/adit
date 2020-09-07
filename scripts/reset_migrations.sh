b58e96ce71c4a7c50ba1cf75b1c8a3d341214979
Kai Schlamp <kai.schlamp@gmail.com>
2020-09-07 17:25:50 +0000

    wip

--- scripts/reset_migrations.sh
+++ scripts/reset_migrations.sh
@@ -2,8 +2,8 @@
 
 # TODO delete if not needed anymore cause of reset_migrations command
 
-find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
-find . -path "*/migrations/*.pyc" -delete
+find . -path "./adit/*/migrations/*.py" -not -name "__init__.py" -delete
+find . -path "./adit/*/migrations/*.pyc" -delete
 python manage.py reset_db --noinput
 python manage.py makemigrations
 python manage.py migrate
