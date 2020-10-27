314ed61b504c09d1c9ebfc4d09d729766915d580
Kai Schlamp <kai.schlamp@gmail.com>
2020-10-27 23:19:00 +0100

    wip

--- adit/main/tests/test_tasks.py
+++ adit/main/tests/test_tasks.py
@@ -1,31 +1,35 @@
-from django.test import TestCase
+import pytest
 from ..factories import TransferTaskFactory, TransferJobFactory, DicomServerFactory
 from ..models import TransferJob, TransferTask
 from ..tasks import transfer_dicoms
 
 
-class TransferDicomsTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.server_to_server_job = TransferJobFactory(
-            status=TransferJob.Status.PENDING,
-            source=DicomServerFactory(),
-            destination=DicomServerFactory(),
-        )
-        cls.study_task = TransferTaskFactory(
-            patient_id=10001,
-            study_uid="1.2.840.113845.11.1000000001951524609.20200705182951.2689481",
-            series_uids=[],
-            pseudonym="",
-            status=TransferTask.Status.PENDING,
-            message="",
-        )
+@pytest.fixture
+def server_to_server_job(db):
+    return TransferJobFactory(
+        status=TransferJob.Status.PENDING,
+        source=DicomServerFactory(),
+        destination=DicomServerFactory(),
+    )
 
-    def test_transfer_study_to_server_succeeds(self):
-        pass
-        # # Arrange
 
-        # # Act
-        # transfer_dicoms(self.study_task.id)
+@pytest.fixture
+def study_task(db):  # pylint: disable=unused-argument
+    return TransferTaskFactory(
+        patient_id=10001,
+        study_uid="1.2.840.113845.11.1000000001951524609.20200705182951.2689481",
+        series_uids=[],
+        pseudonym="",
+        status=TransferTask.Status.PENDING,
+        message="",
+    )
 
-        # # Assert
+
+def test_transfer_study_to_server_succeeds(server_to_server_job, study_task):
+    assert True == True
+    # # Arrange
+
+    # # Act
+    # transfer_dicoms(self.study_task.id)
+
+    # # Assert
