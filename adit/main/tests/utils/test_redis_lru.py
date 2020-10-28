71ac7a8ec9ded0611409cfae6d2b696fe157b18f
Kai Schlamp <kai.schlamp@gmail.com>
2020-10-28 20:19:37 +0100

    Completely switched to pytest

--- adit/main/tests/utils/test_redis_lru.py
+++ adit/main/tests/utils/test_redis_lru.py
@@ -1,67 +1,68 @@
-from django.test import TestCase
 from django.conf import settings
+import pytest
 import redis
 from adit.main.utils.redis_lru import redis_lru
 
 
-class RedisLruTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        redis_url = settings.REDIS_URL[:-1] + "7"
-        cls.redis_client = redis.Redis.from_url(redis_url)
+@pytest.fixture
+def redis_client():
+    redis_url = settings.REDIS_URL[:-1] + "7"
+    return redis.Redis.from_url(redis_url)
 
-    def test_lru_caches_successfully(self):
-        times_called = [0]
 
-        @redis_lru()
-        def func(x, y):
-            times_called[0] += 1
-            return x + y
+def test_lru_caches_successfully(redis_client):
+    times_called = [0]
 
-        func.init(self.redis_client)
+    @redis_lru()
+    def func(x, y):
+        times_called[0] += 1
+        return x + y
 
-        result = func(1, 2)
-        self.assertEqual(result, 3)
-        self.assertEqual(times_called, [1])
+    func.init(redis_client)
 
-        result = func(1, 2)
-        self.assertEqual(result, 3)
-        self.assertEqual(times_called, [1])
+    result = func(1, 2)
+    assert result == 3
+    assert times_called == [1]
 
-        (hits, misses, capacity, size) = func.info()
+    result = func(1, 2)
+    assert result == 3
+    assert times_called == [1]
 
-        self.assertEqual(hits, 1)
-        self.assertEqual(misses, 1)
-        self.assertEqual(capacity, 5000)
-        self.assertEqual(size, 1)
+    (hits, misses, capacity, size) = func.info()
 
-        func.clear()
+    assert hits == 1
+    assert misses == 1
+    assert capacity == 5000
+    assert size == 1
 
-    def test_lru_caches_successfully_with_slicer(self):
-        times_called = [0]
+    func.clear()
 
-        @redis_lru(slicer=slice(2))
-        def func(x, y, z):
-            times_called[0] += 1
-            return x + y + z
 
-        func.init(self.redis_client)
+def test_lru_caches_successfully_with_slicer(redis_client):
+    times_called = [0]
 
-        result = func(1, 2, 3)
-        self.assertEqual(result, 6)
-        self.assertEqual(times_called, [1])
+    @redis_lru(slicer=slice(2))
+    def func(x, y, z):
+        times_called[0] += 1
+        return x + y + z
 
-        # The third value is not considered the second time as
-        # a slicer is used (two only check the first two values).
-        result = func(1, 2, "z")
-        self.assertEqual(result, 6)
-        self.assertEqual(times_called, [1])
+    func.init(redis_client)
 
-        (hits, misses, capacity, size) = func.info()
+    result = func(1, 2, 3)
+    assert result == 6
+    assert times_called == [1]
 
-        self.assertEqual(hits, 1)
-        self.assertEqual(misses, 1)
-        self.assertEqual(capacity, 5000)
-        self.assertEqual(size, 1)
+    # The third value is not considered the second time as
+    # a slicer is used (two only check the first two values).
+    result = func(1, 2, "z")
+    assert result == 6
+    assert times_called == [1]
 
-        func.clear()
+    (hits, misses, capacity, size) = func.info()
+
+    assert hits == 1
+    assert misses == 1
+    assert capacity == 5000
+    assert size == 1
+
+    func.clear()
