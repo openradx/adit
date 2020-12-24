2692b6d5ce03ee4614e779ee5310bdb09ed69b29
medihack <kai.schlamp@gmail.com>
2020-12-24 17:26:48 +0100

    wip

--- adit/batch_transfer/templatetags/batch_transfer_extras.py
+++ adit/batch_transfer/templatetags/batch_transfer_extras.py
@@ -26,6 +26,8 @@ def task_status_badge_class(status):
         css_class = "badge-dark"
     elif status == TransferTask.Status.SUCCESS:
         css_class = "badge-success"
+    elif status == TransferTask.Status.WARNING:
+        css_class = "badge-warning"
     elif status == TransferTask.Status.FAILURE:
         css_class = "badge-danger"
     return css_class
