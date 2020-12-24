9a23bccfdc6e08e3a15c02f2a83baffca2e6dd5b
medihack <kai.schlamp@gmail.com>
2020-12-24 19:33:36 +0100

    wip

--- adit/core/urls.py
+++ adit/core/urls.py
@@ -3,6 +3,11 @@ from django.views.generic import TemplateView
 from .views import FlowerProxyView
 
 urlpatterns = [
+    path(
+        "admin-section/",
+        TemplateView.as_view(template_name="core/admin_section.html"),
+        name="admin_section",
+    ),
     path("", TemplateView.as_view(template_name="core/home.html"), name="home"),
     FlowerProxyView.as_url(),
 ]
