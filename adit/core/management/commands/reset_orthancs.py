42ba1f064ffb51f48b79d6bfe22dc2e78ac4d07a
medihack <kai.schlamp@gmail.com>
2020-12-26 06:53:08 +0100

    wip

--- adit/core/management/commands/reset_orthancs.py
+++ adit/core/management/commands/reset_orthancs.py
@@ -1,8 +1,7 @@
 from os import environ
-from pathlib import Path
-import requests
 from django.core.management.base import BaseCommand
 from django.conf import settings
+from adit.core.utils.orthanc_utils import OrthancRestHandler
 
 
 class Command(BaseCommand):
@@ -17,74 +16,8 @@ class Command(BaseCommand):
         handler = OrthancRestHandler(host=orthanc1_host, port=6501)
         handler.clear()
 
-        handler.upload_files(dicoms_folder)
+        handler.upload(dicoms_folder)
 
         orthanc2_host = environ.get("ORTHANC2_HOST", "localhost")
         handler = OrthancRestHandler(host=orthanc2_host, port=6502)
         handler.clear()
-
-
-class OrthancRestHandler:
-    PATIENTS_RESOURCE = "patients"
-    STUDIES_RESOURCE = "studies"
-    SERIES_RESOURCE = "series"
-    INSTANCES_RESOURCE = "instances"
-
-    def __init__(self, host="localhost", port=8042):
-        self.host = host
-        self.port = port
-
-        # Force requests to not use any set proxy (by http_proxy env variable).
-        # Orthancs for testing are always in the same local network.
-        self.session = requests.Session()
-        self.session.trust_env = False
-
-    def upload_files(self, folder_to_upload):
-        for path in Path(folder_to_upload).rglob("*"):
-            if not path.is_file():
-                continue
-
-            with open(path, "rb") as f:
-                payload = f.read()
-                r = self.session.post(
-                    f"http://{self.host}:{self.port}/instances", data=payload
-                )
-                r.raise_for_status()
-
-    def list(self, resource_type=PATIENTS_RESOURCE):
-        r = self.session.get(f"http://{self.host}:{self.port}/{resource_type}")
-        r.raise_for_status()
-        return r.json()
-
-    def delete(self, res_ids, resource_type=PATIENTS_RESOURCE):
-        for res_id in res_ids:
-            r = self.session.delete(
-                f"http://{self.host}:{self.port}/{resource_type}/{res_id}"
-            )
-            r.raise_for_status()
-
-    def clear(self):
-        res_ids = self.list()
-        self.delete(res_ids)
-
-    def find(self, query=None):
-        if query is None:
-            query = {}
-        r = self.session.post(f"http://{self.host}:{self.port}/tools/find", json=query)
-        r.raise_for_status()
-        return r.json()
-
-
-# Demo of OrthancRestHandler:
-# parent_folder = os.path.dirname(os.path.realpath(__file__))
-# dicom_folder = Path(parent_folder) / "samples" / "dicoms"
-# handler = OrthancRestHandler(port=6501)
-# handler.upload_files(dicom_folder)
-# r = handler.find({
-#     'Level' : OrthancRestHandler.STUDIES_RESOURCE,
-#     'Query' : {
-#         'Modality' : 'MR'
-#     },
-#     'Expand': True
-# })
-# handler.clear_orthanc()
---
+++ adit/core/utils/orthanc_utils.py
@@ -0,0 +1,72 @@
+from pathlib import Path
+import requests
+
+
+class OrthancRestHandler:
+    PATIENTS_RESOURCE = "patients"
+    STUDIES_RESOURCE = "studies"
+    SERIES_RESOURCE = "series"
+    INSTANCES_RESOURCE = "instances"
+
+    def __init__(self, host="localhost", port=8042):
+        self.host = host
+        self.port = port
+
+        # Force requests to not use any set proxy (by http_proxy env variable).
+        # Orthancs for testing are always in the same local network.
+        self.session = requests.Session()
+        self.session.trust_env = False
+
+    def upload(self, folder_to_upload):
+        for path in Path(folder_to_upload).rglob("*"):
+            if not path.is_file():
+                continue
+
+            with open(path, "rb") as f:
+                payload = f.read()
+                r = self.session.post(
+                    f"http://{self.host}:{self.port}/instances", data=payload
+                )
+                r.raise_for_status()
+
+    def list(self, resource_type=STUDIES_RESOURCE):
+        r = self.session.get(f"http://{self.host}:{self.port}/{resource_type}")
+        r.raise_for_status()
+        return r.json()
+
+    def delete(self, res_ids, resource_type=STUDIES_RESOURCE):
+        for res_id in res_ids:
+            r = self.session.delete(
+                f"http://{self.host}:{self.port}/{resource_type}/{res_id}"
+            )
+            r.raise_for_status()
+
+    def clear(self, full_clear=False):
+        res_ids = self.list(resource_type=self.PATIENTS_RESOURCE)
+        self.delete(res_ids, resource_type=self.PATIENTS_RESOURCE)
+
+        if full_clear:
+            r = self.session.delete(f"http://{self.host}:{self.port}/changes")
+            r.raise_for_status()
+            r = self.session.delete(f"http://{self.host}:{self.port}/exports")
+            r.raise_for_status()
+
+    def find(self, query):
+        r = self.session.post(f"http://{self.host}:{self.port}/tools/find", json=query)
+        r.raise_for_status()
+        return r.json()
+
+
+# Demo of OrthancRestHandler:
+# parent_folder = os.path.dirname(os.path.realpath(__file__))
+# dicom_folder = Path(parent_folder) / "samples" / "dicoms"
+# handler = OrthancRestHandler(port=6501)
+# handler.upload(dicom_folder)
+# r = handler.find({
+#     'Level' : OrthancRestHandler.STUDIES_RESOURCE,
+#     'Query' : {
+#         'Modality' : 'MR'
+#     },
+#     'Expand': True
+# })
+# handler.clear_orthanc()
