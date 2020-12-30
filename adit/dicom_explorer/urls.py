613c57bca23550a09338fd6071fcce361d517335
Kai Schlamp <kai.schlamp@gmail.com>
2020-12-30 17:34:55 +0100

    wip

--- adit/dicom_explorer/urls.py
+++ adit/dicom_explorer/urls.py
@@ -1,11 +1,27 @@
-from django.urls import path
+from django.urls import path, re_path
 
-from .views import dicom_explorer_view
+from .views import dicom_explorer_form_view, dicom_explorer_query_view
 
 urlpatterns = [
-    path(
-        "",
-        dicom_explorer_view,
-        name="dicom_explorer",
+    path("", dicom_explorer_form_view, name="dicom_explorer_form"),
+    re_path(
+        r"servers/((?P<server_id>\d+)/)?$",
+        dicom_explorer_query_view,
+        name="dicom_explorer_query_servers",
+    ),
+    re_path(
+        r"servers/(?P<server_id>\d+)/patients/(?:(?P<patient_id>\d+)/)?$",
+        dicom_explorer_query_view,
+        name="dicom_explorer_query_patients",
+    ),
+    re_path(
+        r"servers/(?P<server_id>\d+)/studies/(?:(?P<study_uid>\d+)/)?$",
+        dicom_explorer_query_view,
+        name="dicom_explorer_query_studies",
+    ),
+    re_path(
+        r"servers/(?P<server_id>\d+)/studies/(?P<study_uid>\d+)/series/(?:(?P<series_uid>\d+)/)?$",
+        dicom_explorer_query_view,
+        name="dicom_explorer_query_series",
     ),
 ]
--- adit/dicom_explorer/views.py
+++ adit/dicom_explorer/views.py
@@ -1,9 +1,58 @@
 import asyncio
 from asgiref.sync import sync_to_async
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import DicomExplorerQueryForm
 from .utils.dicom_data_collector import DicomDataCollector
+
+
+@login_required
+def dicom_explorer_form_view(request):
+    if request.GET:
+        form = DicomExplorerQueryForm(request.GET)
+    else:
+        form = DicomExplorerQueryForm()
+
+    if not request.GET or not form.is_valid():
+        return render(
+            request, "dicom_explorer/dicom_explorer_query_form.html", {"form": form}
+        )
+
+    query = form.cleaned_data
+    server = query["server"]
+    patient_id = query.get("patient_id")
+    accession_number = query.get("accession_number")
+    study_uid = query.get("study_uid")
+    series_uid = query.get("series_uid")
+
+    if patient_id and not (accession_number or study_uid):
+        resource_type = "patients"
+    elif (accession_number or study_uid) and not series_uid:
+        resource_type = "studies"
+    elif series_uid:
+        resource_type = "series"
+    else:
+        # Should never happen as we validate the form
+        raise AssertionError("Invalid DICOM explorer query.")
+
+    redirect("dicom_explorer_query", server_id=server.id, resource_type=resource_type)
+
+
+async def dicom_explorer_query_view(request, server_id, resource_type):
+    denied_response = await check_permission(request)
+    if denied_response:
+        return denied_response
+
+    form = await get_form(request)
+    form_valid = await sync_to_async(form.is_valid)()
+
+    if request.GET and form_valid:
+        loop = asyncio.get_event_loop()
+        response = await loop.run_in_executor(None, render_query_result, request, form)
+    else:
+        response = await render_query_form(request, form)
+
+    return response
 
 
 @sync_to_async
@@ -11,15 +60,6 @@ from .utils.dicom_data_collector import DicomDataCollector
 def check_permission(request):
     # A dummy function for the permission decorators
     pass
-
-
-@sync_to_async
-def get_form(request):
-    if not request.GET:
-        form = DicomExplorerQueryForm(initial=request.GET)
-    else:
-        form = DicomExplorerQueryForm(request.GET)
-    return form
 
 
 def render_query_result(request, form):
@@ -84,25 +124,3 @@ def render_query_result(request, form):
 
     # Should never happen as we validate the query with the form instance
     raise AssertionError(f"Invalid DICOM explorer query: {query}")
-
-
-@sync_to_async
-def render_query_form(request, form):
-    return render(request, "dicom_explorer/query_form.html", {"form": form})
-
-
-async def dicom_explorer_view(request):
-    denied_response = await check_permission(request)
-    if denied_response:
-        return denied_response
-
-    form = await get_form(request)
-    form_valid = await sync_to_async(form.is_valid)()
-
-    if request.GET and form_valid:
-        loop = asyncio.get_event_loop()
-        response = await loop.run_in_executor(None, render_query_result, request, form)
-    else:
-        response = await render_query_form(request, form)
-
-    return response
