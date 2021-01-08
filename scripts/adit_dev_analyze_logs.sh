c5528042389881ecec3753a99bfcd48ea661039a
medihack <kai.schlamp@gmail.com>
2021-01-08 19:16:57 +0100

    wip

---
+++ scripts/adit_dev_analyze_logs.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run --rm -v adit_dev_syslog_data:/mnt/adit_logs -w /mnt/adit_logs -it ubuntu /bin/bash
--- scripts/adit_dev_investigate_syslog.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run --rm -v adit_dev_syslog_data:/mnt/adit_logs -w /mnt/adit_logs -it kalilinux/kali /bin/bash
