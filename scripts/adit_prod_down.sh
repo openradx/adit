bd7dff950f496e6298866f6c0cba6ac94c327978
Kai Schlamp <kai.schlamp@gmail.com>
2021-01-13 15:33:20 +0100

    wip

---
+++ scripts/adit_prod_down.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+PROJECT_DIR="$(dirname $(dirname $(readlink -f $0)))"
+
+docker-compose -f "$PROJECT_DIR/compose/adit_prod/docker-compose.prod.yml" down --remove-orphans
---
+++ scripts/adit_prod_shell.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+PROJECT_DIR="$(dirname $(dirname $(readlink -f $0)))"
+
+docker-compose -f compose/adit_prod/docker-compose.prod.yml exec web python manage.py shell_plus
