cf13494eef3733d16b18d83633402ee942e8cdfb
Manuel Debić <manuel.debic@med.uni-heidelberg.de>
2021-11-12 13:26:06 +0100

    Remove uneccessary dcm4chee test pacs

--- scripts/dcm4chee_pacs_dev_down.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-PROJECT_DIR="$(dirname $(dirname $(readlink -f $0)))"
-
-docker-compose -p dcm4chee -f "$PROJECT_DIR/compose/dcm4chee_pacs_dev/docker-compose.yml" down --remove-orphans "$@"
