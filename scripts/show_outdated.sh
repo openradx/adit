72323a7243bea1e7e270a84013eb1416c2c1021f
Kai Schlamp <kai.schlamp@gmail.com>
2022-04-07 15:28:34 +0200

    Improve check for updates script

--- scripts/show_outdated.sh
+++ scripts/show_outdated.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+echo "Python packages:"
 poetry show --outdated | grep --file=<(poetry show --tree | grep '^\w' | sed 's/^\([^ ]*\).*/^\1/')
+
+echo "Javascript packages:"
+npm outdated
