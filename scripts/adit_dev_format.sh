050c3417f7b50d793c96deb29cfcc39ff1a48ae3
Kai Schlamp <kai.schlamp@gmail.com>
2022-11-03 16:27:48 +0000

    Add script to format complete code

---
+++ scripts/adit_dev_format.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+PROJECT_DIR="$(dirname $(dirname $(readlink -f $0)))"
+
+docker-compose -f "$PROJECT_DIR/compose/adit_dev/docker-compose.dev.yml" exec web black ./adit "$@"
+docker-compose -f "$PROJECT_DIR/compose/adit_dev/docker-compose.dev.yml" exec web isort ./adit "$@"
