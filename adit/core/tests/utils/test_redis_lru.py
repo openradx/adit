127eee361feb05dc100564788e84672fe3960142
Kai Schlamp <kai.schlamp@gmail.com>
2022-11-10 12:33:55 +0000

    Remove ADIT_FULLSTACK env variable

--- adit/core/tests/utils/test_redis_lru.py
+++ adit/core/tests/utils/test_redis_lru.py
@@ -10,7 +10,7 @@ def redis_client():
     return redis.Redis.from_url(redis_url)
 
 
-@pytest.mark.skipif(not settings.ADIT_FULLSTACK, reason="Needs a running Redis server.")
+@pytest.mark.integration
 def test_lru_caches_successfully(redis_client):
     times_called = [0]
 
@@ -39,7 +39,7 @@ def test_lru_caches_successfully(redis_client):
     func.clear()
 
 
-@pytest.mark.skipif(not settings.ADIT_FULLSTACK, reason="Needs a running Redis server.")
+@pytest.mark.integration
 def test_lru_caches_successfully_with_slicer(redis_client):
     times_called = [0]
 
