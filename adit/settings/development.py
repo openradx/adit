84407a4486e3ebbe77614bfa9a574c70504954f0
Kai Schlamp <kai.schlamp@gmail.com>
2022-11-10 10:51:32 +0000

    Show all debug toolbar panels (as bug seems to be fixed)

--- adit/settings/development.py
+++ adit/settings/development.py
@@ -29,26 +29,6 @@ INSTALLED_APPS += [  # noqa: F405
 
 MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa: F405
 
-DEBUG_TOOLBAR_PANELS = [
-    "debug_toolbar.panels.versions.VersionsPanel",
-    "debug_toolbar.panels.timer.TimerPanel",
-    "debug_toolbar.panels.settings.SettingsPanel",
-    "debug_toolbar.panels.headers.HeadersPanel",
-    "debug_toolbar.panels.request.RequestPanel",
-    "debug_toolbar.panels.sql.SQLPanel",
-    "debug_toolbar.panels.staticfiles.StaticFilesPanel",
-    "debug_toolbar.panels.templates.TemplatesPanel",
-    "debug_toolbar.panels.cache.CachePanel",
-    "debug_toolbar.panels.signals.SignalsPanel",
-    # LoggingPanel does not work with Django Channels, see
-    # https://github.com/django/channels/issues/1204
-    # https://github.com/jazzband/django-debug-toolbar/issues/1300
-    # "debug_toolbar.panels.logging.LoggingPanel",
-    "debug_toolbar.panels.redirects.RedirectsPanel",
-    "debug_toolbar.panels.profiling.ProfilingPanel",
-]
-
-
 if env.bool("FORCE_DEBUG_TOOLBAR", default=False):
     DEBUG_TOOLBAR_CONFIG = {"SHOW_TOOLBAR_CALLBACK": lambda request: DEBUG}
 
