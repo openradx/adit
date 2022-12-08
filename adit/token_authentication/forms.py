c003a9d5d1bd3b44eb3e75dc204c03019035a455
Sander <sander@sander-ThinkPad-E490>
2022-12-08 10:50:15 +0100

    improved tests for rest_api and xnat_support

---
+++ adit/token_authentication/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+
+
+
+class GenerateRestAuthTokenForm(forms.Form):
+    EXPIRY_TIMES = (
+        (1/60, "1 Minute"),
+        (1, "1 Hour"),
+        (24, "1 Day"),
+        (168, "7 Days"),
+        (720, "30 Days"),
+    )
+    expiry_time = forms.ChoiceField(
+        choices=EXPIRY_TIMES,
+        required=True,
+        label="Expiry Time"
+    )
+    client = forms.CharField(
+        max_length=64,
+        required=True,
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.helper = FormHelper(self)
+        self.helper.form_id = "token_form"
