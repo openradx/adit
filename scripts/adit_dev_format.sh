f8f3218c341e0cc35c89bd8acd114aa4f0b834a7
Kai Schlamp <kai.schlamp@gmail.com>
2023-03-22 12:22:11 +0000

    Improve format script

--- scripts/adit_dev_format.sh
+++ scripts/adit_dev_format.sh
@@ -2,5 +2,11 @@
 
 source "$(dirname "$0")/common.sh"
 
+# Format Python code
 eval $COMPOSE_COMMAND_DEV exec web black ./adit
-eval $COMPOSE_COMMAND_DEV exec web isort ./adit
+
+# Sort Python imports
+eval $COMPOSE_COMMAND_DEV exec web ruff . --fix --select I
+
+# Format Django templates
+eval $COMPOSE_COMMAND_DEV exec web djlint . --reformat
