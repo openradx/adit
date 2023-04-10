14dd24bd004f0492637f7c0fcdc0054725949338
Kai Schlamp <kai.schlamp@gmail.com>
2023-04-10 19:30:59 +0200

    Add Github codespaces support (#30)
    * Add new devcontainer file for Github Codespaces (not working yet)
    
    * Script to setup codespaces

---
+++ scripts/init_codespaces.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+source "$(dirname "$0")/common.sh"
+
+ENV_DEV_FILE="$COMPOSE_DIR/.env.dev"
+
+cp -a $PROJECT_DIR/example.env $ENV_DEV_FILE
+
+BASE_URL="https://${CODESPACE_NAME}-8000.preview.app.github.dev"
+sed -i "s#\(BASE_URL=\).*#\1$BASE_URL#" $ENV_DEV_FILE
+sed -i "s#\(DJANGO_CSRF_TRUSTED_ORIGINS=\).*#\1$BASE_URL#" $ENV_DEV_FILE
+
+HOST=${BASE_URL#https://}
+sed -i "s#DJANGO_ALLOWED_HOSTS=#&$HOST,#" $ENV_DEV_FILE
+sed -i "s#DJANGO_INTERNAL_IPS=#&$HOST,#" $ENV_DEV_FILE
+
+sed -i "s#\(FORCE_DEBUG_TOOLBAR=\).*#\1true#" $ENV_DEV_FILE
