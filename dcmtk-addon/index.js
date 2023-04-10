29483e202c2da7995a0c08cf3e136678e3a047a1
Kai Schlamp <kai.schlamp@gmail.com>
2023-04-10 15:33:09 +0000

    wip

--- dcmtk-addon/index.js
+++ dcmtk-addon/index.js
@@ -1,4 +1,3 @@
-// var addon = require("bindings")("dcmconnector-addon");
 const addon = require("./build/Release/dcmconnector-addon");
 
 console.log(addon.hello()); // 'world'
--- dcmtk/do.sh
+++ dcmtk/do.sh
@@ -2,8 +2,8 @@
 
 rm -rf ./build/
 sudo apt-get -y install libsndfile1-dev libwrap0-dev libopenjp2-7-dev doxygen
-export DCMDICTPATH=/workspace/dcmtk-3.6.7-install/usr/local/share/dcmtk/dicom.dic
 cmake -S. -Bbuild -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 cmake --build build --target all
+export DCMDICTPATH=/workspace/dcmtk-3.6.7-install/usr/local/share/dcmtk/dicom.dic
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/workspace/dcmtk-3.6.7-install/usr/local/lib/
 ./build/demo
