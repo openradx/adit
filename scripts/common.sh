7d497108457b32530fbae486db44fa9092e80ede
Kai Schlamp <kai.schlamp@gmail.com>
2023-04-17 10:57:20 +0000

    Another try to solve Codespaces docker in docker problem

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+// https://github.com/microsoft/vscode-remote-release/issues/7958
+if [ "$CODESPACES" = true ]; then
+    export DOCKER_BUILDKIT=0
+    export BUILDKIT_INLINE_CACHE=0
+fi
+
 ADIT_DEV_PROJ="adit_dev"
 ADIT_PROD_PROJ="adit_prod"
 
@@ -10,4 +16,3 @@ COMPOSE_DIR="$PROJECT_DIR/compose"
 COMPOSE_COMMAND_BASE="docker compose -f '$COMPOSE_DIR/docker-compose.base.yml'"
 COMPOSE_COMMAND_DEV="$COMPOSE_COMMAND_BASE -f '$COMPOSE_DIR/docker-compose.dev.yml' -p $ADIT_DEV_PROJ"
 COMPOSE_COMMAND_PROD="$COMPOSE_COMMAND_BASE -f '$COMPOSE_DIR/docker-compose.prod.yml' -p $ADIT_PROD_PROJ"
- 
