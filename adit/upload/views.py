8445f1e1a56d2afb9b945927befae49da11b488e
Chris Heidt <vanchriz1-github@yahoo.de>
2023-06-29 14:27:13 +0000

    Working Upload commit before rebase dev

--- adit/upload/views.py
+++ adit/upload/views.py
@@ -16,19 +16,34 @@ class UploadJobCreateView(DicomJobCreateView):
     template_name = "upload/upload_job_form.html"
     permission_required = "batch_transfer.add_batchtransferjob"
 
+    def handle_uploaded_file(self, f, folder):
+        directory = "/tmp/" + folder
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        with open(directory + "/" + f.name, "wb+") as destination:
+            for chunk in f.chunks():
+                destination.write(chunk)
+
     def form_valid(self, form):
         user = self.request.user
         form.instance.owner = user
 
         response = super().form_valid(form)
-
-        upload_folder = "upload" + str(uuid.uuid1())
-        job = self.object
-        job.source = DicomFolder(path=upload_folder)
-            
+        debug_number_of_files = -1
+        debug_path = "nopath"
         if form.is_valid():
+            debug_number_of_files = len(self.request.FILES.getlist('upload_files'))
+            debug_path = form.cleaned_data.get('source').path
             for f in self.request.FILES.getlist('upload_files'):
-                handle_uploaded_file(f, upload_folder)
+                self.handle_uploaded_file(f, form.cleaned_data.get('source').path)
+
+        job = self.object
+        upload_folder = job.source.path
+        #job.source = DicomFolder(path=upload_folder)
+            
+        #if form.is_valid():
+        #    for f in self.request.FILES.getlist('upload_files'):
+        #        handle_uploaded_file(f, upload_folder)
 
         
         if user.is_staff or settings.BATCH_TRANSFER_UNVERIFIED:
@@ -38,10 +53,5 @@ class UploadJobCreateView(DicomJobCreateView):
 
         return response
 
-    def handle_uploaded_file(f, folder):
-        directory = "/tmp/" + folder
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-        with open(directory + "/" + f.name, "wb+") as destination:
-            for chunk in f.chunks():
-                destination.write(chunk)
+    def get_success_url(self):
+        return self.request.path
