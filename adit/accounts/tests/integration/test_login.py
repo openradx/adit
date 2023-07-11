78b2190b402951869a71b37e5caea3fba16056a6
Kai Schlamp <kai.schlamp@gmail.com>
2023-07-11 15:28:45 +0200

    Change login test

--- adit/accounts/tests/integration/test_login.py
+++ adit/accounts/tests/integration/test_login.py
@@ -6,5 +6,4 @@ from playwright.sync_api import Page, expect
 @pytest.mark.django_db(transaction=True)
 def test_login(page: Page, live_server, create_and_login_user):
     user = create_and_login_user(live_server.url)
-
-    expect(page.locator("#navbarDropdown")).to_have_text(user.username)
+    expect(page.get_by_text(user.username)).to_be_visible()
