c85f37db6ae113959542db57b39c05923a9b4ae3
Kai Schlamp <kai.schlamp@gmail.com>
2023-07-13 18:39:24 +0000

    Fix migration tests by not using factories

--- adit/batch_transfer/tests/test_migrations.py
+++ adit/batch_transfer/tests/test_migrations.py
@@ -3,17 +3,39 @@
 import pytest
 from django_test_migrations.migrator import Migrator
 
-from ..factories import BatchTransferJobFactory
-
 
 @pytest.mark.django_db
 def test_0013_convert_json_to_text(migrator: Migrator):
     old_state = migrator.apply_initial_migration(
         ("batch_transfer", "0012_alter_batchtransfertask_lines_and_more")
     )
+
+    # Historical models are reconstructed from the migration files. We can't use our factories here.
+    User = old_state.apps.get_model("accounts", "User")
+    DicomServer = old_state.apps.get_model("core", "DicomServer")
+    BatchTransferJob = old_state.apps.get_model("batch_transfer", "BatchTransferJob")
     BatchTransferTask = old_state.apps.get_model("batch_transfer", "BatchTransferTask")
 
-    job = BatchTransferJobFactory.create()
+    server1 = DicomServer.objects.create(
+        ae_title="server1",
+        name="server1",
+        host="server1",
+        port=11112,
+    )
+    server2 = DicomServer.objects.create(
+        ae_title="server2",
+        name="server2",
+        host="server2",
+        port=11112,
+    )
+    user = User.objects.create(
+        username="user",
+    )
+    job = BatchTransferJob.objects.create(
+        source_id=server1.id,
+        destination_id=server2.id,
+        owner_id=user.id,
+    )
     task = BatchTransferTask.objects.create(
         job_id=job.id,
         task_id="123",
--- adit/selective_transfer/tests/test_migrations.py
+++ adit/selective_transfer/tests/test_migrations.py
@@ -3,17 +3,39 @@
 import pytest
 from django_test_migrations.migrator import Migrator
 
-from ..factories import SelectiveTransferJobFactory
-
 
 @pytest.mark.django_db
 def test_0012_convert_json_to_text(migrator: Migrator):
     old_state = migrator.apply_initial_migration(
         ("selective_transfer", "0011_alter_selectivetransfertask_series_uids")
     )
+
+    # Historical models are reconstructed from the migration files. We can't use our factories here.
+    User = old_state.apps.get_model("accounts", "User")
+    DicomServer = old_state.apps.get_model("core", "DicomServer")
+    SelectiveTransferJob = old_state.apps.get_model("selective_transfer", "SelectiveTransferJob")
     SelectiveTransferTask = old_state.apps.get_model("selective_transfer", "SelectiveTransferTask")
 
-    job = SelectiveTransferJobFactory.create()
+    server1 = DicomServer.objects.create(
+        ae_title="server1",
+        name="server1",
+        host="server1",
+        port=11112,
+    )
+    server2 = DicomServer.objects.create(
+        ae_title="server2",
+        name="server2",
+        host="server2",
+        port=11112,
+    )
+    user = User.objects.create(
+        username="user",
+    )
+    job = SelectiveTransferJob.objects.create(
+        source_id=server1.id,
+        destination_id=server2.id,
+        owner_id=user.id,
+    )
     task = SelectiveTransferTask.objects.create(
         job_id=job.id,
         task_id="123",
