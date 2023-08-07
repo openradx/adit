ab88f455ec458fe63a30685cd192ee6762383b62
Kai Schlamp <kai.schlamp@gmail.com>
2023-08-07 15:10:46 +0200

    Show batch query results in Django admin

--- adit/batch_query/admin.py
+++ adit/batch_query/admin.py
@@ -2,9 +2,26 @@ from django.contrib import admin
 
 from adit.core.admin import DicomJobAdmin, DicomTaskAdmin
 
-from .models import BatchQueryJob, BatchQuerySettings, BatchQueryTask
+from .models import BatchQueryJob, BatchQueryResult, BatchQuerySettings, BatchQueryTask
+
+
+class BatchQueryResultAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "job_id",
+        "query_id",
+        "patient_id",
+        "study_date",
+        "modalities",
+        "study_description",
+        "series_description",
+        "pseudonym",
+    )
+
 
 admin.site.register(BatchQueryJob, DicomJobAdmin)
+
+admin.site.register(BatchQueryResult, BatchQueryResultAdmin)
 
 admin.site.register(BatchQueryTask, DicomTaskAdmin)
 
