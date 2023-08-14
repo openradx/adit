802efa097eacf87ee40271c655f1bf34dafe371b
Kai Schlamp <kai.schlamp@gmail.com>
2023-08-14 17:11:12 +0000

    Fix qido series test

--- adit/dicom_web/tests/integration/test_qidors.py
+++ adit/dicom_web/tests/integration/test_qidors.py
@@ -1,5 +1,9 @@
+import re
+
 import pydicom
 import pytest
+from dicomweb_client import DICOMwebClient
+from requests import HTTPError
 
 
 @pytest.mark.integration
@@ -37,15 +41,15 @@ def test_qido_series(
     create_dicom_web_client,
     extended_data_sheet,
 ):
-    orthanc1_client = create_dicom_web_client(channels_live_server.url, "ORTHANC1")
+    orthanc1_client: DICOMwebClient = create_dicom_web_client(channels_live_server.url, "ORTHANC1")
 
-    results = orthanc1_client.search_for_series()
-    results_series_uids = set()
-    for result_json in results:
-        results_series_uids.add(pydicom.Dataset.from_json(result_json).SeriesInstanceUID)
-    assert results_series_uids == set(
-        extended_data_sheet["SeriesInstanceUID"]
-    ), "Basic QIDO request on series level failed"
+    try:
+        # Even DICOMweb standard does allow this we don't support it querying all series
+        # without a StudyInstanceUID, because it is not possible with a connected DIMSE server.
+        orthanc1_client.search_for_series()
+    except HTTPError as err:
+        error_details = err.response.json()
+        assert re.search("StudyInstanceUID is required", error_details[0])
 
     study_uid = list(extended_data_sheet["StudyInstanceUID"])[0]
     results = orthanc1_client.search_for_series(study_uid)
@@ -58,18 +62,11 @@ def test_qido_series(
         ]
     ), "QIDO request with StudyInstanceUID on series level failed"
 
-    results = orthanc1_client.search_for_series()
-    results_series_uids = set(
-        [pydicom.Dataset.from_json(result_json).SeriesInstanceUID for result_json in results]
-    )
-    assert results_series_uids == set(
-        extended_data_sheet["SeriesInstanceUID"]
-    ), "QIDO request with empty StudyInstanceUID on series level failed"
-
-    results = orthanc1_client.search_for_series(search_filters={"Modality": "MR"})
-    results_series_uids = set()
+    results = orthanc1_client.search_for_series(study_uid, search_filters={"Modality": "SR"})
+    results_series_uids = list()
     for result_json in results:
-        results_series_uids.add(pydicom.Dataset.from_json(result_json).SeriesInstanceUID)
-    assert results_series_uids == set(
-        extended_data_sheet[extended_data_sheet["Modality"] == "MR"]["SeriesInstanceUID"]
-    ), "QIDO request with Modality filter on series level failed"
+        results_series_uids.append(pydicom.Dataset.from_json(result_json).SeriesInstanceUID)
+    assert len(results_series_uids) == 1
+    assert (
+        results_series_uids[0] == list(extended_data_sheet["SeriesInstanceUID"])[3]
+    ), "QIDO request with StudyInstanceUID and Modality filter on series level failed"
