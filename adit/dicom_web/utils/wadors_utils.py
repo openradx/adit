3703a039ab115cf9d009fc2802b7804ce1d7c4e2
Kai Schlamp <kai.schlamp@gmail.com>
2023-08-22 17:13:31 +0200

    Rename local variable

--- adit/dicom_web/utils/wadors_utils.py
+++ adit/dicom_web/utils/wadors_utils.py
@@ -13,16 +13,16 @@ async def wado_retrieve(
     query: dict[str, str],
     dest_folder: PathLike,
 ) -> None:
-    connector = DicomOperator(source_server)
+    operator = DicomOperator(source_server)
     query_ds = QueryDataset.from_dict(query)
 
-    series_list = list(await sync_to_async(connector.find_series)(query_ds))
+    series_list = list(await sync_to_async(operator.find_series)(query_ds))
 
     if len(series_list) == 0:
         raise NotFound("No DICOM objects matches the query.")
 
     for series in series_list:
-        await sync_to_async(connector.download_series)(
+        await sync_to_async(operator.download_series)(
             patient_id=series.PatientID,
             study_uid=series.StudyInstanceUID,
             series_uid=series.SeriesInstanceUID,
