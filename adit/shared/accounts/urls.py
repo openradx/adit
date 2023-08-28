61b980105d4e776642c66992401feeb19e609d3c
Kai Schlamp <kai.schlamp@gmail.com>
2023-08-28 18:45:37 +0000

    Delete shared folder

--- adit/shared/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import include, path
-
-from . import views
-
-urlpatterns = [
-    path("", include("registration.backends.admin_approval.urls")),
-    path("profile/", views.UserProfileView.as_view(), name="profile"),
-]
