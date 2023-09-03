05d36097c212442e2086814ed966656bb9d6a093
Kai Schlamp <kai.schlamp@gmail.com>
2023-09-03 08:16:11 +0000

    Move registration templates to global templates folder

--- adit/settings/base.py
+++ adit/settings/base.py
@@ -93,7 +93,7 @@ ROOT_URLCONF = "adit.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "adit" / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
