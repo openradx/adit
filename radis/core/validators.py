d3bb87a082190806b8212f1dbf51deb23f8eea39
Kai Schlamp <kai.schlamp@gmail.com>
2023-09-13 19:57:43 +0000

    Remove unused validators

--- radis/core/validators.py
+++ radis/core/validators.py
@@ -1,11 +1,6 @@
 from django.core.exceptions import ValidationError
 from django.core.validators import RegexValidator
 
-pos_int_list_validator = RegexValidator(
-    regex=r"^\s*\d+(?:\s*,\s*\d+)*\s*\Z",
-    message="Enter only digits separated by commas.",
-)
-
 no_backslash_char_validator = RegexValidator(
     regex=r"\\",
     message="Contains invalid backslash character",
@@ -24,17 +19,6 @@ no_wildcard_chars_validator = RegexValidator(
     message="Contains invalid wildcard characters.",
     inverse_match=True,
 )
-
-uid_chars_validator = RegexValidator(regex=r"^[\d\.]+$", message="Invalid character in UID.")
-
-
-def validate_uids(value: str) -> None:
-    uids = map(str.strip, value.split(","))
-    for uid in uids:
-        if len(uid) > 64:
-            raise ValidationError("UID string too long (max 64 characters).")
-
-        uid_chars_validator(uid)
 
 
 def validate_modalities(value: str) -> None:
