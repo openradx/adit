ece65965e3d93825a7fd95e6182d82159415980e
Kai Schlamp <kai.schlamp@gmail.com>
2023-10-23 09:35:00 +0000

    Make sure Email is unique during registration
    (cherry picked from commit 0da034d2b6e0330a51ab47cb795c44c1bba14c0f)

--- radis/accounts/forms.py
+++ radis/accounts/forms.py
@@ -3,6 +3,7 @@ from typing import Any
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.forms import ValidationError
 
 from .models import User
 
@@ -34,9 +35,21 @@ class RegistrationForm(UserCreationForm):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
 
+        # In Django's AbstractUser (which django-registration-redux uses internally) the fields
+        # email, first name and last name are not required. We fix this in our registration
+        # form.
         self.fields["email"].required = True
         self.fields["first_name"].required = True
         self.fields["last_name"].required = True
 
         self.helper = FormHelper(self)
         self.helper.add_input(Submit("register", "Register"))
+
+    def clean_email(self):
+        # Django's AbstractUser Email model field is not unique. We fix this here programmatically
+        # at the form level.
+        email: str = self.cleaned_data["email"]
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("An account with this Email address is already registered.")
+
+        return email
