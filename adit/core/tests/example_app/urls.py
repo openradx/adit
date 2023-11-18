407bcaf85b882a85fc07f436d32f9553241bae8a
Kai Schlamp <kai.schlamp@gmail.com>
2023-11-18 11:49:58 +0000

    Fix job delete view and add more view tests

--- adit/core/tests/example_app/urls.py
+++ adit/core/tests/example_app/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 
-from .views import ExampleTransferJobCancelView
+from .views import ExampleTransferJobCancelView, ExampleTransferJobDeleteView
 
 urlpatterns = [
+    path(
+        "jobs/<int:pk>/delete/",
+        ExampleTransferJobDeleteView.as_view(),
+        name="example_transfer_job_delete",
+    ),
     path(
         "jobs/<int:pk>/cancel/",
         ExampleTransferJobCancelView.as_view(),
         name="example_transfer_job_cancel",
-    )
+    ),
 ]
--- adit/core/tests/example_app/views.py
+++ adit/core/tests/example_app/views.py
@@ -1,6 +1,11 @@
-from adit.core.views import DicomJobCancelView
+from adit.core.views import DicomJobCancelView, DicomJobDeleteView
 
 from .models import ExampleTransferJob
+
+
+class ExampleTransferJobDeleteView(DicomJobDeleteView):
+    model = ExampleTransferJob
+    success_url = "/"
 
 
 class ExampleTransferJobCancelView(DicomJobCancelView):
