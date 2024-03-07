de2602bf28eb8ddd2a07a90b616977243a01bc85
Kai Schlamp <kai.schlamp@gmail.com>
2024-03-07 17:00:42 +0100

    Improve some logging configuration

--- adit/celery.py
+++ adit/celery.py
@@ -22,6 +22,12 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 from celery.signals import setup_logging  # noqa: E402
 
 
+# When setting up logging like this then CELERY_WORKER_HIJACK_ROOT_LOGGER
+# has no effect anymore as it Celery will never try to hijack it.abs
+# It will be forced to take the logger configuration from our Django
+# settings. The downside is that we don't get the Celery logger
+# (get_task_logger) anymore that prints out the Celery task ID with
+# the log message.
 @setup_logging.connect
 def config_loggers(*args, **kwargs):
     from logging.config import dictConfig
