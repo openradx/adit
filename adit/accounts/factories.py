74e679195638c818383fcfff1e5c4a585d447027
Kai Schlamp <kai.schlamp@gmail.com>
2024-03-22 22:42:51 +0000

    Fix warning about post generation calls of factory boy
    Factory boy won't save the instance in post generation hooks in the next major version. I fixed this here, but the warning still remains (I guess more as a hint). So we still ignore the warning and remove this ignore when the next major version of factory boy gets released.

--- adit/accounts/factories.py
+++ adit/accounts/factories.py
@@ -1,4 +1,4 @@
-from typing import Generic, TypeVar
+from typing import Generic, TypeVar, cast
 
 import factory
 from django.contrib.auth.models import Group
@@ -22,19 +22,39 @@ class UserFactory(BaseDjangoModelFactory[User]):
 
     username = factory.Sequence(lambda n: f"user_{n}")
     email = factory.Faker("email")
-    password = factory.PostGenerationMethodCall("set_password", "userpass")
     first_name = factory.Faker("first_name")
     last_name = factory.Faker("last_name")
     phone_number = factory.Faker("phone_number")
     department = factory.Faker("company")
 
+    @factory.post_generation
+    def password(obj, create, extracted, **kwargs):
+        user = cast(User, obj)
+        if extracted:
+            user.set_password(extracted)
+        else:
+            user.set_password("userpass")
+
+        if create:
+            user.save()
+
 
 class AdminUserFactory(UserFactory):
     username = "admin"
     email = "admin@adit.test"
-    password = factory.PostGenerationMethodCall("set_password", "admin")
     is_superuser = True
     is_staff = True
+
+    @factory.post_generation
+    def password(obj, create, extracted, **kwargs):
+        user = cast(User, obj)
+        if extracted:
+            user.set_password(extracted)
+        else:
+            user.set_password("admin")
+
+        if create:
+            user.save()
 
 
 class GroupFactory(BaseDjangoModelFactory[Group]):
