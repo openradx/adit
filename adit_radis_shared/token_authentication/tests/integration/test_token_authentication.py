03436f2d1aebefb475be787179d6c468e3783af8
Kai Schlamp <kai.schlamp@gmail.com>
2024-03-29 13:10:39 +0000

    Force transaction when using live_server

--- adit_radis_shared/token_authentication/tests/integration/test_token_authentication.py
+++ adit_radis_shared/token_authentication/tests/integration/test_token_authentication.py
@@ -6,24 +6,24 @@ from adit_radis_shared.common.utils.auth_utils import add_user_to_group
 
 
 @pytest.mark.integration
-@pytest.mark.django_db
+@pytest.mark.django_db(transaction=True)
 def test_create_and_delete_authentication_token(
     page: Page,
-    channels_live_server,
+    live_server,
     create_and_login_user,
     token_authentication_group,
 ):
-    user = create_and_login_user(channels_live_server.url)
+    user = create_and_login_user(live_server.url)
     add_user_to_group(user, token_authentication_group)
 
-    page.goto(channels_live_server.url + "/token-authentication/")
+    page.goto(live_server.url + "/token-authentication/")
     page.get_by_label("Description").fill("Just a test token")
     page.get_by_text("Generate Token").click()
     expect(page.locator("#unhashed-token-string")).to_be_visible()
     token = page.locator("#unhashed-token-string").inner_text()
 
     response = requests.get(
-        channels_live_server.url + "/token-authentication/test",
+        live_server.url + "/token-authentication/test",
         headers={"Authorization": f"Token {token}"},
     )
     assert response.status_code == 200
@@ -33,19 +33,19 @@ def test_create_and_delete_authentication_token(
     expect(page.locator("table").get_by_text("Just a test token")).not_to_be_visible()
 
     response = requests.get(
-        channels_live_server.url + "/token-authentication/test",
+        live_server.url + "/token-authentication/test",
         headers={"Authorization": f"Token {token}"},
     )
     assert response.status_code == 401
 
 
 @pytest.mark.integration
-@pytest.mark.django_db
+@pytest.mark.django_db(transaction=True)
 def test_invalid_authentication_token(
-    channels_live_server,
+    live_server,
 ):
     response = requests.get(
-        channels_live_server.url + "/token-authentication/test",
+        live_server.url + "/token-authentication/test",
         headers={"Authorization": "Token invalid_token"},
     )
     assert response.status_code == 401
