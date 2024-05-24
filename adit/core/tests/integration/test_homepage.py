c85ca608deba45d19eca6c39df9fbb5b2d51bcd3
Kai Schlamp <kai.schlamp@gmail.com>
2024-05-24 22:09:58 +0000

    wip

--- adit/core/tests/integration/test_homepage.py
+++ adit/core/tests/integration/test_homepage.py
@@ -6,6 +6,7 @@ from playwright.sync_api import Page, expect
 
 @pytest.mark.integration(transaction=True)
 def test_homepage_has_title(live_server, page: Page):
+    print(live_server.url)
     page.goto(live_server.url)
 
     # Expect a title "to contain" a substring.
