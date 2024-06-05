564fbd5f69f0ca0dbe74a0207d5b92fcc674be08
LucaDulz <lucamdulz@gmail.com>
2024-06-05 15:38:58 +0000

    delete upload mixin

--- adit/upload/mixins.py
+++
@@ -1,9 +0,0 @@
-from adit.core.mixins import LockedMixin
-
-from .apps import SECTION_NAME
-from .models import UploadSettings
-
-
-class UploadLockedMixin(LockedMixin):
-    settings_model = UploadSettings
-    section_name = SECTION_NAME
