c257f0cf3d9506bf9084aaa97356bed9d65b34ec
LucaDulz <lucamdulz@gmail.com>
2024-09-02 08:04:35 +0000

    cleanup for CI

--- adit/upload/models.py
+++ adit/upload/models.py
@@ -9,9 +9,14 @@ class UploadSettings(DicomAppSettings):
 
 
 class UploadPermissionSupport(models.Model):
+    id: int
+
     class Meta:
         managed = False
         default_permissions = ()
         permissions = [
             ("can_upload_data", "Can upload data"),
         ]
+
+    def __str__(self) -> str:
+        return f"{self.__class__.__name__} [ID {self.id}]"
