3dc574a85500a2178bb3647f0046b4dc5a4a0397
LucaDulz <lucamdulz@gmail.com>
2024-10-10 14:57:48 +0000

    integrate UploadPermissionSupport into existing UploadSettings

--- adit/upload/models.py
+++ adit/upload/models.py
@@ -1,20 +1,12 @@
-from django.db import models
-
 from adit.core.models import DicomAppSettings
 
 
 class UploadSettings(DicomAppSettings):
     class Meta:
         verbose_name_plural = "Upload settings"
-
-
-class UploadPermissionSupport(models.Model):
-    class Meta:
-        managed = False
         default_permissions = ()
         permissions = [
             ("can_upload_data", "Can upload data"),
         ]
 
-    def __str__(self) -> str:
-        return f"{self.__class__.__name__} [{self.pk}]"
+
