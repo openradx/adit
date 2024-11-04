e99944fcd98acbdd50079c78b956aca6bf8882fc
Kai Schlamp <kai.schlamp@gmail.com>
2024-11-04 23:40:03 +0000

    Remove special handling of INTERNAL_IPS for Docker
    This is not needed anymore as it is done on the server side using the middleware of Django Debug Toolbar, see https://github.com/django-commons/django-debug-toolbar/blob/2c66ff848d3ba9acd2bb4538c32f6aab05d41ce2/debug_toolbar/middleware.py#L34

--- adit/settings/development.py
+++ adit/settings/development.py
@@ -26,13 +26,4 @@ if env.bool("FORCE_DEBUG_TOOLBAR"):
 
 LOGGING["loggers"]["adit"]["level"] = "DEBUG"  # noqa: F405
 
-if env.bool("USE_DOCKER"):
-    import socket
-
-    # For Debug Toolbar to show up on Docker Compose in development mode.
-    # This only works when browsed from the host where the containers are run.
-    # If viewed from somewhere else then DJANGO_INTERNAL_IPS must be set.
-    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
-    INTERNAL_IPS += [".".join(ip.split(".")[:-1] + ["1"]) for ip in ips]  # noqa: F405
-
 ENABLE_DICOM_DEBUG_LOGGER = False
