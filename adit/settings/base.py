7369f436bc9beaa48c2255d945d3ebf1cd4808f4
Sumantra Sharma <sumantra94@gmail.com>
2025-07-23 14:22:58 +0000

    adding test settings

--- adit/settings/base.py
+++ adit/settings/base.py
@@ -331,10 +331,6 @@ CALLING_AE_TITLE = env.str("CALLING_AE_TITLE")
 # The AE title where the receiver is listening for incoming files.
 RECEIVER_AE_TITLE = env.str("RECEIVER_AE_TITLE")
 
-# Path to sample DICOM files, used only for testing purposes.
-# This shouldn't be needed in production environments.
-DICOM_SAMPLE_DIR = env.str("DICOM_SAMPLE_DIR", str(BASE_PATH / "samples" / "dicoms"))
-
 # The address and port of the STORE SCP server (part of the receiver).
 # By default the STORE SCP server listens to all interfaces (empty string)
 STORE_SCP_HOST = env.str("STORE_SCP_HOST", "localhost")
