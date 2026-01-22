bc4d804eccc0142ac27e69510195e32af5fef170
Samuel Kwong <samvictor232@gmail.com>
2026-01-22 13:06:51 +0000

    add debugging of dicom datetime elements

--- adit/core/utils/pseudonymizer.py
+++ adit/core/utils/pseudonymizer.py
@@ -1,12 +1,55 @@
 import logging
 import traceback
 
+import pydicom
 from dicognito.anonymizer import Anonymizer
+from dicognito.element_anonymizer import ElementAnonymizer
 from dicognito.value_keeper import ValueKeeper
 from django.conf import settings
 from pydicom import Dataset
 
 logger = logging.getLogger(__name__)
+
+
+class DateTimeLoggingHandler(ElementAnonymizer):
+    """
+    A logging handler that inspects and logs datetime DICOM elements
+    before they are shifted by the DateTimeAnonymizer.
+    """
+
+    def __call__(self, dataset: Dataset, data_element: pydicom.DataElement) -> bool:
+        """
+        Log datetime elements (DA and DT) for debugging purposes.
+
+        Returns False so that the actual DateTimeAnonymizer can still process the element.
+        """
+        if data_element.VR not in ("DA", "DT"):
+            return False
+
+        # Get related time element if this is a date element
+        time_value = None
+        if data_element.VR == "DA":
+            time_name = data_element.keyword.replace("Date", "Time")
+            if time_name in dataset:
+                time_element = dataset.data_element(time_name)
+                if time_element and time_element.value:
+                    time_value = time_element.value
+
+        logger.debug(
+            "DEBUG DateTimeShift: Element %s (tag: %s, VR: %s) = %r%s",
+            data_element.keyword,
+            data_element.tag,
+            data_element.VR,
+            data_element.value,
+            f", corresponding Time ({time_name}) = {time_value!r}" if time_value else "",
+        )
+
+        # Return False to allow the DateTimeAnonymizer to process the element
+        return False
+
+    def describe_actions(self):
+        """Describe the actions this handler performs."""
+        yield "Log all DA and DT elements before datetime shifting"
 
 
 class Pseudonymizer:
@@ -31,6 +74,10 @@ class Pseudonymizer:
         anonymizer = Anonymizer()
         for element in settings.SKIP_ELEMENTS_ANONYMIZATION:
             anonymizer.add_element_handler(ValueKeeper(element))
+
+        # Add datetime logging handler for debugging datetime shifts
+        anonymizer.add_element_handler(DateTimeLoggingHandler())
+
         return anonymizer
 
     def pseudonymize(self, ds: Dataset, pseudonym: str) -> None:
